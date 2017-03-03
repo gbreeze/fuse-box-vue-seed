20156cbb7a619e018842f059691d1890985bc604
Georg Perhofer <georg.perhofer@aimsoftware.com>
2017-03-03 18:16:49 +0100

    error: https://github.com/vuejs/vue-router/issues/713

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 //import * as Vue from 'vue/dist/vue.js';
 import * as Vue from 'vue';
-import { MyComponent } from './view'
 //import * as MyComponent from './view';
 import './main.scss';
 
 new Vue({
-    el: '#app',
-    components: {
-        'myComponent': MyComponent
-    }
-}) 
+    el: '#app'
+})
+
+
+//// error
+// https://github.com/vuejs/vue-router/issues/713
