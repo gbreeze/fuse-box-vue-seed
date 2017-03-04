39fc954a0eb18a8fe8e26430df88c6c0b684b085
George Breeze <mal@georgebreeze.com>
2017-03-04 08:18:28 +0100

    vue alias added

--- fuse.js
+++ fuse.js
@@ -6,6 +6,9 @@ const fuseBox = fsbx.FuseBox.init({
         bundleReference: 'app.js.map',
         outFile: './dist/app.js.map'
     },
+    alias: {
+        'vue': 'vue/dist/vue.common.js'
+    },
     outFile: './dist/app.js',
     plugins: [
         [
@@ -15,7 +18,8 @@ const fuseBox = fsbx.FuseBox.init({
         fsbx.TypeScriptHelpers(),
         fsbx.JSONPlugin(),
         fsbx.HTMLPlugin({ useDefault: false })
-    ]
+    ],
+    cache: false
 });
 
 fuseBox.devServer('>main.ts **/*.html', {
